e15b8cd5ae4274b6ba5fc15c46062f433436bd14
yihudaojia <86771848+yihudaojia@users.noreply.github.com>
2021-08-10 20:57:07 +0800

    update

--- iOS/WebApp/ViewController.m
+++ iOS/WebApp/ViewController.m
@@ -6,39 +6,15 @@
 //
 
 #import "ViewController.h"
-#import <WebKit/WebKit.h>
-
-@interface ViewController ()
-
-@property(nonatomic, strong) WKWebView *webView;
-
-@end
+#import "WebViewController.h"
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    NSURL *url = [NSURL URLWithString:@"https://mi.yihu365.cn"];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-
-    WKPreferences *prefs = [[WKPreferences alloc] init];
-    prefs.javaScriptEnabled = true;
-    
-    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
-    config.preferences = prefs;
-    
-    _webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
-    _webView.allowsBackForwardNavigationGestures = true;
-
-    [_webView loadRequest:request];
-    [self.view addSubview:_webView];
-}
-
-- (void)viewDidLayoutSubviews {
-    [super viewDidLayoutSubviews];
-    
-    _webView.frame = self.view.bounds;
+- (IBAction)openWeb:(id)sender
+{
+    WebViewController *vc = [WebViewController new];
+    vc.url = @"https://mi.yihu365.cn";
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 
